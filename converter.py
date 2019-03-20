b935183e001cf8c1de8fa81d93c1d1ade540f2a7
Kevin Hunyadi <kevin.hunyadi@gmail.com>
2019-03-20 15:58:10 +0100

    Finalizing a lot of functionality. TODO: write some user friendly gui/wrapper or something

--- converter.py
+++ converter.py
@@ -14,6 +14,8 @@ class Converter(object):
         self.file_list = self.excel.get_rows()
         self.discogs_client = Discogs()
         self.filtered_list = []
+        self.full_properties_list = []
+        self.no_properties_found_list = []
 
     def filter_doubles(self):
         last_file = None
@@ -36,51 +38,127 @@ class Converter(object):
             search_term = filename.split('.', 1)[0]
             # Convert underscores to spaces
             search_term = search_term.replace("_", " ")
-
+            search_term = search_term.replace("-", " ")
+            search_term = search_term.replace("'", "")
             # remove trailing numbers or spaces.
             search_term = re.sub(r'^[ \d\.]*', '', search_term)
             file.append(search_term)
 
-    def convert_to_full_properties(self):
-        self.full_properties_list = []
-        for file in self.filtered_list:
-            results = self.discogs_client.search_release(file[2])
-            print(file[2])
-            print(results.pages)
-
-            # Get the first result, we feel always lucky
-            if not results:
-                # Nothing found for this search term, continueing
-                continue
-            release = results[0]
-            tracks = [track.title for track in release.tracklist]
-            best_matches = get_close_matches(file[2], tracks)
-            if not best_matches:
-                # Try again with less match terms.
+    def find_track_in_tracklist(self, file, tracks):
+        best_matches = get_close_matches(file[2], tracks)
+        if not best_matches:
+            # Try again with less match terms.
+            filename_without_extension = file[0].split('.', 1)[0]
+            if '_' in filename_without_extension:
+                # Split into pieces
+                new_search_terms = filename_without_extension.split('_')
+                for search_term in new_search_terms:
+                    best_matches = get_close_matches(search_term, tracks)
+                    if best_matches:
+                        break
+            elif '-' in filename_without_extension:
+                # split into pieces
+                new_search_terms = filename_without_extension.split('-')
+                for search_term in new_search_terms:
+                    best_matches = get_close_matches(search_term, tracks)
+                    if best_matches:
+                        break
+            else:
+                # last resort, just split into pieces with spaces
                 new_search_terms = file[2].split(' ')
                 for search_term in new_search_terms:
                     best_matches = get_close_matches(search_term, tracks)
                     if best_matches:
                         break
 
-            if best_matches:
-                track_title = best_matches[0]
-                for track in release.tracklist:
-                    if track.title == track_title:
-                        break
+        if best_matches:
+            track_title = best_matches[0]
+            return track_title
+        return False
 
+    def find_properties_with_item(self, file):
+        results = self.discogs_client.search_release(file[2])
+        print(file[2])
+        print(results.pages)
+
+        # Get the first result, we feel always lucky
+        if not results:
+            # Try with the search term without parentheses content.
+            results = self.discogs_client.search_release(re.sub(r'\(.*\)', '', file[2]))
+
+            if not results:
+                # Nothing found for this search term, return an empty properties dict with only the filename
+                print("Didn't find any results from discogs")
                 properties = {}
-                properties['Titel'] = track.title
-                properties['Uitvoerder'] = ",".join([artist.name for artist in release.artists])
-                properties['Componist'] = ",".join([artist.name for artist in track.credits])
-                if not properties['Componist']:
-                    properties['Componist'] = ",".join([artist.name for artist in release.credits])
-                properties['Duurtijd'] = file[1].get_in_minutes_seconds()
-                properties['Label'] = ",".join([label.name for label in release.labels])
-                print(properties)
+                properties['Titel'] = file[0]
+                properties['Uitvoerder'] = ''
+                properties['Componist'] = ''
+                properties['Duurtijd'] = ''
+                properties['Label'] = ''
+                return properties
+
+        release = results[0]
+        tracks = [track.title for track in release.tracklist]
+        track_name = self.find_track_in_tracklist(file, tracks)
+
+        if not track_name:
+            # If there's still no track found. It might be because the tracklist contains text between () which we don't like!
+            # get this text out and repeat.
+            tracks = [re.sub(r'\(.*\)', '', track.title) for track in release.tracklist]
+            track_name = self.find_track_in_tracklist(file, tracks)
+            if not track_name:
+                # Nothing found for this search term, return an empty properties dict with only the filename
+                print("Didn't find the correct track!")
+                properties = {}
+                properties['Titel'] = file[0]
+                properties['Uitvoerder'] = ''
+                properties['Componist'] = ''
+                properties['Duurtijd'] = ''
+                properties['Label'] = ''
+                return properties
+            else:
+                for track in release.tracklist:
+                    track_title = re.sub(r'\(.*\)', '', track.title)
+                    if track_title == track_name:
+                        break
+        else:
+            for track in release.tracklist:
+                if track.title == track_name:
+                    break
+
+        properties = {}
+        properties['Titel'] = track.title
+        properties['Uitvoerder'] = ",".join(set([artist.name for artist in release.artists]))
+        # Get all the writers
+        writers = []
+        if 'extraartists' in track.data.keys():
+            for extra_artist in track.data['extraartists']:
+                if extra_artist['role'] == 'Written-By':
+                    writers.append(extra_artist['name'])
+            # if no writers found on the track. Let's get the writers of the release
+        if 'extraartists' in release.data.keys():
+            if not writers:
+                for extra_artist in release.data['extraartists']:
+                    if extra_artist['role'] == 'Written-By':
+                        writers.append(extra_artist['name'])
+
+        properties['Componist'] = ",".join(set(writers))
+        properties['Duurtijd'] = file[1].get_in_minutes_seconds()
+        properties['Label'] = ",".join(set([label.name for label in release.labels]))
+        print(properties)
+
+        return properties
+
+    def convert_to_full_properties(self):
+        for file in self.filtered_list:
+            print(file[0])
+            properties = self.find_properties_with_item(file)
+            if properties:
                 self.full_properties_list.append(properties)
             else:
-                print("Didn't find a single match in the track list, I'm sorry")
+                # No properties found. Append it to another list.
+                self.no_properties_found_list.append(file)
+
             # Sleep 1 second because the discogs api doesn't like us
             time.sleep(1)
 
--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from converter import Converter
 
-converter = Converter('C:\\Users\\kevin\\Downloads\\TVS_AFL01.xlsx')
+converter = Converter('/home/khunyadi/Downloads/EDL-AFL-06.xlsx')
 converter.filter_doubles()
 converter.add_search_terms()
 converter.convert_to_full_properties()
-converter.export_to_csv('C:\\Users\\kevin\\Downloads\\SABAM AFL1.csv')
+converter.export_to_csv('/home/khunyadi/Downloads/SABAM-AFL-06.csv')
